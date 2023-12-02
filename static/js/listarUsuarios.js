ed306a9aeebec75d31e60e3b5c5270dfa0675008
ezequielmz <93398008+ezequielmz@users.noreply.github.com>
2023-12-02 01:04:55 -0300

    feat: se agregaron los modales de agregar, modificar y eliminar usuario fix: se corrigieron problemas con la carga de datos en los modales Notas: Modal agregar_usuario funciona, todos los modales fallan en la verificación de datos,       fallas en entrega de mensajes de error.

--- static/js/listarUsuarios.js
+++ static/js/listarUsuarios.js
@@ -1,192 +1,70 @@
-const URL = "http://127.0.0.1:5000/"
-// const URL = "https://ezequielmz.pythonanywhere.com/"
+const URL = "http://127.0.0.1:5000/";
 
 // Realizamos la solicitud GET al servidor para obtener todos los productos
 fetch(URL + 'usuarios')
-    .then(function (response) {
-        if (response.ok) {return response.json(); }
-    })
-    .then(function (data) {
-        let tablaUsuarios = document.getElementById('tablaUsuarios');
+  .then(function (response) {
+    if (response.ok) {
+      return response.json();
+    } else {
+      // Manejar el error de respuesta del servidor
+      console.error('Error al obtener los usuarios:', response.status);
+      // Mostrar un mensaje de error al usuario
+      alert('Error al obtener los usuarios. Intenta nuevamente.');
+    }
+  })
+  .then(function (data) {
+    let tablaUsuarios = document.getElementById('tablaUsuarios');
 
-        // Iteramos sobre los usuarios y agregamos filas a la tabla
-        for (let usuario of data) {
-            let fila = document.createElement('tr');
-            fila.innerHTML = 
-                '<td>' + usuario.id + '</td>' +
-                '<td>' + usuario.nombre + '</td>' +
-                '<td>' + usuario.apellido + '</td>' +
-                '<td>' + usuario.password + '</td>' +
-                '<td>' + usuario.email + '</td>' +
-                '<td>' + usuario.telefono + '</td>';
-            tablaUsuarios.appendChild(fila);
-        }
-    })
-    .catch(function (error) {
-        // Código para manejar errores
-        //alert('Error al obtener los usuarios.');
-    });
-
-
-
-								// 	<td>{{ usuario.id }}</td>
-								// 	<td>{{ usuario.nombre }}</td>
-								// 	<td>{{ usuario.apellido }}</td>
-								// 	<td>{{ usuario.password }}</td>
-								// 	<td>{{ usuario.email }}</td>
-								// 	<td>{{ usuario.telefono }}</td>
-								// 	<td>
-
-								// 	<a href='#' data-bs-toggle='modal' data-bs-target='#modificarUsuario'><i class='bi bi-pencil-fill m-1'></i></a>
-
-								// 	<a href='#' @click="eliminar_usuario(usuario.id)" data-bs-toggle='modal' data-bs-target='#delModal'><i class='bi bi-trash-fill m-1'></i></a>
-								// </td>
-						
+    // Iteramos sobre los usuarios y agregamos filas a la tabla
+    for (let usuario of data.usuarios) {
+      let fila = document.createElement('tr');
+      fila.innerHTML =
+        `<td>${usuario.id}</td>
+         <td>${usuario.nombre}</td>
+         <td>${usuario.apellido}</td>
+         <td>${usuario.password}</td>
+         <td>${usuario.email}</td>
+         <td>${usuario.telefono}</td>
+         <td>
+         <a href='#' data-bs-toggle='modal' data-bs-target='#modificarUsuario' onclick='cargarDatosModal(${JSON.stringify(usuario)})'><i class='bi bi-pencil-fill m-1'></i></a>
+            <a href='#' data-bs-toggle='modal' data-bs-target='#delModal' onclick='cargarDataUser(${JSON.stringify(usuario)})'><i class='bi bi-trash-fill m-1'></i></a>
+         </td>`;
+      tablaUsuarios.appendChild(fila);
+    }
+  })
+  .catch(function (error) {
+    // Código para manejar errores de red o de conexión
+    console.error('Error de red o de conexión:', error);
+    alert('Error de red o de conexión. Verifica tu conexión a Internet.');
+  });
 
 
+//Función para cargar datos en el modal
+  function cargarDatosModal(usuario) {
+    document.getElementById('nombre_mod').value = usuario.nombre;
+    document.getElementById('apellido_mod').value = usuario.apellido;
+    document.getElementById('contrasena_mod').value = usuario.password;
+    document.getElementById('email_mod').value = usuario.email;
+    document.getElementById('telefono_mod').value = usuario.telefono;
+  }
 
+// Función para cargar datos de usuario a eliminar
+function cargarDataUser(usuario) {
+    let dataUsuario = document.getElementById('dataUser')
+    dataUsuario.innerHTML = " " + usuario.nombre + " " + usuario.apellido;
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// URL = "http://127.0.0.1:5000/";
-// // appTabla
-// const app = new Vue({
-//   el: "#app",
-//   data: {
-//     usuarios: [],
-//   },
-//   mounted() {
-//     // Al cargar la página, obtenemos la lista de usuarios
-//     this.listarUsuarios();
-//   },
-//   methods: {
-//     async listarUsuarios() {
-//       try {
-//         const response = await fetch(URL + "usuarios");
-//         const data = await response.json();
-//         this.usuarios = data.usuarios;
-//       } catch (error) {
-//         console.error("Error:", error);
-//         alert("Error al obtener la lista de usuarios.");
-//       }
-//     },
-//   },
-
-//   template: `
-      // <div>
-      //     <table class="table table-striped text-center">
-      //         <thead class="table-dark">
-      //             <tr>
-      //                 <th scope="col">#ID</th>
-      //                 <th scope="col">Nombre</th>
-      //                 <th scope="col">Apellido</th>
-      //                 <th scope="col">Password</th>
-      //                 <th scope="col">Email</th>
-      //                 <th scope="col">Teléfono</th>
-      //             </tr>
-      //         </thead>
-      //         <tbody>
-      //             <tr v-for="usuario in usuarios" :key="usuario.id">
-      //                 <td>{{ usuario.id }}</td>
-      //                 <td>{{ usuario.nombre }}</td>
-      //                 <td>{{ usuario.apellido }}</td>
-      //                 <td>{{ usuario.password }}</td>
-      //                 <td>{{ usuario.email }}</td>
-      //                 <td>{{ usuario.telefono }}</td>
-      //                 <td>
-
-      //                 <a href='#' data-bs-toggle='modal' data-bs-target='#modificarUsuario'><i class='bi bi-pencil-fill m-1'></i></a>
-
-      //                 <a href='#' @click="eliminar_usuario(usuario.id)" data-bs-toggle='modal' data-bs-target='#delModal'><i class='bi bi-trash-fill m-1'></i></a>
-      //               </td>
-      //             </tr>
-      //         </tbody>
-      //     </table>
-      // </div>
-//   `,
-// });
-
-// id = 2;
-// //appMod
-// const appmod = new Vue({
-//   el: "#appMod",
-//   data: {
-//     usuario_id: {},
-//   },
-//   mounted() {
-//     // Al cargar el modal, obtenemos el  usuario por id
-//     this.consultar_usuario_id(id);
-//   },
-//   methods: {
-//     async consultar_usuario_id(id) {
-//       try {
-//         const response = await fetch(`${URL}usuarios/${id}`);
-//         const data = await response.json();
-//         this.usuario_id = data;
-//       } catch (error) {
-//         console.error("Error:", error);
-//         alert("Error al obtener la lista de usuarios.");
-//       }
-//     },
-//   },
-//   template: `
-// 					// <div class="form-floating mb-3">
-// 					// 	<div id="appmod"></div>
-// 					// 	<input type="hidden" name="id" v-model="usuario_id.id">
-// 					// 	<div class="form-floating mb-3">
-// 					// 		<input type="text" name="name" class="form-control rounded-3" id="name" placeholder="Nombre"
-// 					// 			v-model="usuario_id.nombre" required>
-// 					// 		<label for="name">Nombre</label>
-// 					// 	</div>
-// 					// 	<div class="form-floating mb-3">
-// 					// 		<input type="text" name="lastname" class="form-control rounded-3" id="lastname" placeholder="Apellido"
-// 					// 			v-model="usuario_id.apellido" required>
-// 					// 		<label for="lastname">Apellido</label>
-// 					// 	</div>
-// 					// 	<div class="form-floating mb-3">
-// 					// 		<input type="text" name="email" class="form-control rounded-3" id="email" placeholder="Email"
-// 					// 			v-model="usuario_id.email" required>
-// 					// 		<label for="email">Email</label>
-// 					// 	</div>
-// 					// 	<div class="form-floating mb-3">
-// 					// 		<input type="text" name="tel" class="form-control rounded-3" id="tel" placeholder="Teléfono movil"
-// 					// 			v-model="usuario_id.telefono" required>
-// 					// 		<label for="tel">Teléfono</label>
-// 					// 	</div>
-// 					// 	<div class="form-floating mb-3">
-// 					// 		<input type="text" name="tema" class="form-control rounded-3" id="tema" placeholder="Tema"
-// 					// 			v-model="usuario_id.password" required>
-// 					// 		<label for="tema">Contraseña</label>
-// 					// 	</div>
-//           <div>ESTOY EN JS CON VUE</div>
-//   `,
-// });
-
-
-
+// // Función para cargar datos en el modal
+//   function cargarDatosModal(usuario) {
+//     const modals = document.querySelectorAll('.modal-modificar');
+//     modals.forEach((modal) => {
+//       modal.querySelector('.id').value = usuario.id;
+//       modal.querySelector('.nombre').value = usuario.nombre;
+//       modal.querySelector('.apellido').value = usuario.apellido;
+//       modal.querySelector('.contrasena').value = usuario.password;
+//       modal.querySelector('.email').value = usuario.email;
+//       modal.querySelector('.telefono').value = usuario.telefono;
+//     });
+//   }
+  
+  
